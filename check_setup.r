322aecb89fbb6ae6276e2d551eb19be8b6b1e972
John Peach <githubspam@jppeach.com>
2019-05-11 22:28:36 -0700

    Script to check your R setup

---
+++ check_setup.r
@@ -0,0 +1,24 @@
+# Install needed packages
+package <- c('rstudioapi', 'tidyverse', 'caret')
+lapply(package, FUN = function(pkg) {
+  if (!require(pkg, quietly=TRUE, warn.conflicts=FALSE, character.only = TRUE)){
+    install.packages(pkg, dependencies = TRUE)
+  }
+}) %>% invisible()
+
+# check the version of R
+if (as.numeric(R.Version()$major) < 3 ||
+    (as.numeric(R.Version()$major) == 3 &
+     as.numeric(stringr::str_split(R.Version()$minor, "[.]")[[1]][[1]]) < 4)) {
+  warning('Your version of R is quite old, consider upgrading')
+}
+
+# Check the version of R Studio
+if ( rstudioapi::versionInfo()$version < "1.1") {
+  warning('Using an old version of RStudio, consider upgrading')
+}
+
+
+# Update all packages
+# This may take a while
+update.packages(ask = FALSE)
